fc4edc5f9a8733545f95eafdf1b1c2b64638bbc2
Derric Fragosa <fderric@gmail.com>
2019-12-21 06:27:29 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,9 +15,13 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
  self.split(/[.!?]+/).count
  #self.split(/\.\,|\?|!/).count not passing returns # of sentences in str
    #self.split(/\.|\?|!/).count won't return # of sentence in a complex str test
+=======
+  self.split(/\.\,|\?|!/)
+>>>>>>> 6c4bdb05a450728d4ddc1f270ad1dab04d103ec1
   #.count(0..-1) && .count[0..-1] not work
  # self.grep(/^\w\W\s/) not work
  # binding.pry
